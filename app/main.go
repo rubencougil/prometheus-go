5e330964d59dff857e8694379ad12f95a53b0dea
rubencougil <rubencougil@gmail.com>
2020-07-11 13:22:32 +0200

    update alert rules, fix bug in app

--- app/main.go
+++ app/main.go
@@ -53,7 +53,10 @@ func main() {
 	go func() {
 		for {
 			// Example of metric push
-			push.New("http://pushgateway:9091", "geekshub_job").Collector(e).Add()
+			err := push.New("http://localhost:9091", "geekshub_job").Collector(e).Add()
+			if err != nil {
+				_ = fmt.Errorf("%v", err)
+			}
 			e.Inc()
 			fmt.Print("_")
 			time.Sleep(1 * time.Second)
